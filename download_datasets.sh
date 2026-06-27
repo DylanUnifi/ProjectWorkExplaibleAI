b51c627ff19906ae25e0bd3f53edfbba0d6d7701
DylanUnifi <dylan.fouepe@edu.unifi.it>
2026-06-28 00:56:04 +0200

    Remove bpy from rsbench requirements to fix Python 3.10 install error

--- download_datasets.sh
+++ download_datasets.sh
@@ -23,6 +23,8 @@ if [ ! -d "data/mnmath" ]; then
         git clone https://github.com/unitn-sml/rsbench-code.git
     fi
     cd rsbench-code/rssgen
+    # Remove bpy (Blender Python) from requirements since it's not needed for MNMath and breaks on Python 3.10
+    sed -i '/bpy/d' requirements.txt
     pip install -r requirements.txt
     echo "Generating MNMath dataset... this may take a moment."
     python -m rssgen examples_config/mnist.yml mnist ../../data/mnmath
